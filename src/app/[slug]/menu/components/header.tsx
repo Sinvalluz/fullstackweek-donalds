f31b4c7b043ebd52bbcf2bbd3d6b19a88a667558
Sinval Luz <contato.sinvalluz@gmail.com>
2025-03-31 09:22:45 -0300

    feat: ensure all orders and back buttons work as expected

--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -4,7 +4,7 @@ import { Restaurant } from '@prisma/client';
 import { Pick } from '@prisma/client/runtime/library';
 import { ChevronLeftIcon, ScrollTextIcon } from 'lucide-react';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
 
@@ -13,8 +13,10 @@ interface RestaurantHeaderProps {
 }
 
 const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
+	const { slug } = useParams<{ slug: string }>();
 	const router = useRouter();
 	const handleBackClick = () => router.back();
+	const handleOrdersClick = () => router.push(`/${slug}/orders`);
 	return (
 		<div className='relative h-[250px] w-full'>
 			<Button
@@ -33,7 +35,8 @@ const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
 			<Button
 				variant='secondary'
 				size='icon'
-				className='absolute right-4 top-4 z-50 rounded-full'>
+				className='absolute right-4 top-4 z-50 rounded-full'
+				onClick={handleOrdersClick}>
 				<ScrollTextIcon />
 			</Button>
 		</div>
